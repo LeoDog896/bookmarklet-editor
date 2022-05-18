54ef588d67f3b3aa3b3c5522d870806a5f047291
LeoDog896 <LeoDog896@hotmail.com>
2022-05-18 11:59:57 +0000

    Add codemirror

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { EditorState, basicSetup } from "@codemirror/basic-setup"
+  import { EditorView, keymap } from "@codemirror/view"
+  import { indentWithTab } from "@codemirror/commands"
+  import { javascript } from "@codemirror/lang-javascript"
   import { minify } from "terser";
 
   let jsCode: string = "";
-</script>
+  let parent: HTMLDivElement
 
-<input bind:value={jsCode}/>
+  onMount(() => {
+    new EditorView({
+      state: EditorState.create({
+        jsCode,
+        extensions: [
+          basicSetup,
+          keymap.of([indentWithTab]),
+          javascript()
+        ]
+      }),
+      parent
+    })
+  })
+</script>
+<div bind:this={parent}></div>
 
 {#await minify(jsCode) then minifiedCode}
-  <p>{minifiedCode.code}</p>
+  <a href={"javascript:" + minifiedCode}>javascript:{minifiedCode.code}</a>
 {/await}
