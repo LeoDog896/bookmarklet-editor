efbcddaf544b8deaba464ab615a00e9f027e9d5b
LeoDog896 <LeoDog896@hotmail.com>
2022-05-18 11:36:53 +0000

    Begin bookmarklet editor

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { minify } from "terser";
+
+  let jsCode: string = "";
+</script>
+
+<input bind:value={jsCode}/>
+
+{#await minify(jsCode) then minifiedCode}
+  <p>{minifiedCode.code}</p>
+{/await}
